c7463b89d4f97323d9e64eaa9787d347a8c1eff0
KruASe <kruanse@ya.ru>
2022-07-14 22:18:57 +0300

    ggb_generator.py is approaching...

---
+++ project/ggb_generator.py
@@ -0,0 +1,12 @@
+# TODO: ggb_generator.save(construction, path)
+
+import os
+import shutil
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
+
+from construction import Construction
+
+
+def save(constr: Construction, path: str) -> None:
+    pass
